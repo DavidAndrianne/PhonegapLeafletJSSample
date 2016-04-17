da0c33f837461bc174b7b1c971d3eb2f0fab2c81
DavidAndrianne <r0426942@gmail.com>
2016-04-17 12:12:21 +0200

    Solutions Exercises 1 & 2. nodejs_server made dynamic

---
+++ platforms/browser/www/nodejs_server.js
@@ -0,0 +1,40 @@
+let sys = require("util"),
+my_http = require("http"),
+path = require("path"),
+url = require("url"),
+filesys = require("fs");
+my_http.createServer(function(request,response){
+	console.log("received request!");
+    let my_path = url.parse(request.url).pathname;
+	let full_path = path.join(process.cwd(),my_path);
+	/*path.exists(full_path,function(exists){
+        if(!exists){
+            response.writeHeader(404, {"Content-Type": "text/plain"});  
+            response.write("404 Not Found\n");  
+            response.end();
+        }
+        else{*/
+            filesys.readFile(full_path, "binary", function(err, file) {  
+                 if(err) {  
+					console.log("ERROR : issue loading file :" + full_path);
+					
+					// verbose output
+					/*console.log("request url : "+url.parse(request.url).pathname);
+					  console.log("returned path: "+full_path); */
+					console.log(err);
+                    response.writeHeader(500, {"Content-Type": "text/plain"});  
+                    response.write(err + "\n");  
+                    response.end();  
+                 }  
+                 else{
+					console.log("writing page!");
+                    response.writeHeader(200);  
+                    response.write(file, "binary");  
+                    response.end();
+                }
+                      
+            });
+        //}
+    //});
+}).listen(8080);
+console.log("Server Running on 8080");         
--- www/nodejs_server.js
+++ www/nodejs_server.js
@@ -1,13 +1,13 @@
-var sys = require("util"),
+let sys = require("util"),
 my_http = require("http"),
 path = require("path"),
 url = require("url"),
 filesys = require("fs");
 my_http.createServer(function(request,response){
 	console.log("received request!");
-    var my_path = url.parse(request.url).pathname;
-    var full_path = "C:\\Users\\David\\Documents\\phonegap-template-hello-world-master\\platforms\\browser\\www\\index.html";//path.join(process.cwd(),my_path);
-    /*path.exists(full_path,function(exists){
+    let my_path = url.parse(request.url).pathname;
+	let full_path = path.join(process.cwd(),my_path);
+	/*path.exists(full_path,function(exists){
         if(!exists){
             response.writeHeader(404, {"Content-Type": "text/plain"});  
             response.write("404 Not Found\n");  
@@ -16,14 +16,18 @@ my_http.createServer(function(request,response){
         else{*/
             filesys.readFile(full_path, "binary", function(err, file) {  
                  if(err) {  
+					console.log("ERROR : issue loading file :" + full_path);
+					
+					// verbose output
+					/*console.log("request url : "+url.parse(request.url).pathname);
+					  console.log("returned path: "+full_path); */
 					console.log(err);
-                     response.writeHeader(500, {"Content-Type": "text/plain"});  
-                     response.write(err + "\n");  
-                     response.end();  
-                
+                    response.writeHeader(500, {"Content-Type": "text/plain"});  
+                    response.write(err + "\n");  
+                    response.end();  
                  }  
                  else{
-					 console.log("writing page!");
+					console.log("writing page!");
                     response.writeHeader(200);  
                     response.write(file, "binary");  
                     response.end();
